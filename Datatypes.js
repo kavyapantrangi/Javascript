27806ce720dd424d277817cffcf3a1338414cf43
kavyapantrangi <126671211+kavyapantrangi@users.noreply.github.com>
2024-08-18 00:34:00 +0530

    Create Datatypes.js

---
+++ Datatypes.js
@@ -0,0 +1,29 @@
+Number
+null
+string
+undefined
+boolean################3primitive#############
+bigint
+symbol
+#############referenec##########
+  object
+function
+  array
+object example:
+let person={
+    fName:'kavya',
+    lName:'pantrangi',
+    age:12
+};
+console.log(person);
+console.log(typeof person);####output####:
+  Object
+age
+: 
+12
+fName
+: 
+"kavya"
+lName
+: 
+"
