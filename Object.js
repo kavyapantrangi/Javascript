41123b210ddc8bf616848c14f244fb55ee55db7b
kavyapantrangi <126671211+kavyapantrangi@users.noreply.github.com>
2024-08-22 21:17:22 +0530

    Update Object.js

--- Object.js
+++ Object.js
@@ -25,18 +25,6 @@ console.log("age" in Person);
 console.log(typeof Person);
 
 
-
-
-
-
-
-
-
-
-
-
-
-
 output:
 Kavya
  Pantrangi
@@ -46,3 +34,47 @@ age   12
  address   Object
  true
  object
+
+
+
+
+//Js Objects
+//js object methods
+const Person={
+        fname:"Kavya",
+        Lname:"Pantrangi",
+        age:"12",
+        address:{
+            city:"Banglore",
+            state:"Karnataka",
+            pincode:"523631"
+        },
+        greeet:function()
+        {
+console.log("hello world ",this.fname);
+
+        }
+    }
+    console.log(Person.greeet());
+    //we can also assign outside the object
+    let wish=function hello()
+    {
+        console.log("Hello ,how are u??");
+    }
+Person.wish=wish;
+console.log(Person);
+console.log(Person.wish);
+//this keyword
+//used for the acessing the current object
+console.log(this);
+
+
+Output:
+hello world  Kavya 
+ Object
+ ƒ hello()
+    {
+        console.log("Hello ,how are u??");
+    }
+ Window
+
