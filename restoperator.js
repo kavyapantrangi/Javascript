ce72be747bba4f13cb339d9e3235f16be652ee42
kavyapantrangi <126671211+kavyapantrangi@users.noreply.github.com>
2024-08-28 21:39:50 +0530

    Create restoperator.js

---
+++ restoperator.js
@@ -0,0 +1,8 @@
+Rest operator is used to condense multiple elements into single array or object.
+This is useful when we dont know how many parameters a function may receive
+and you want to capture all of them as an array.
+  Example:
+  function Example(...args){
+  
+console.log(args)
+}
