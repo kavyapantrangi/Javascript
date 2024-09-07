9ccb3374a3685691f4cccbd8764f847aa34397fa
kavyapantrangi <126671211+kavyapantrangi@users.noreply.github.com>
2024-09-07 19:53:20 +0530

    Update EventListeners.js

--- EventListeners.js
+++ EventListeners.js
@@ -20,3 +20,30 @@ myButton.addEventListener("click",()=>
 })
 </script>
 </html>
+
+//styling 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="conatiner">
+        <p id="p1">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rem, dignissimos!</p>
+        <button id="hi">Click me</button>
+        
+    </div>
+</body>
+<script >
+    // styling the content
+const myButton=document.getElementById("hi");
+const para=document.getElementById("p1");
+myButton.addEventListener("click",()=>
+{
+    para.style.backgroundColor='red';
+})
+</script>
+</html>
