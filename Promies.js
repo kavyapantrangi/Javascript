bfb499b64345dde5ba5d5c35c4e0613576256c65
kavyapantrangi <126671211+kavyapantrangi@users.noreply.github.com>
2024-09-12 21:33:23 +0530

    Create Promies.js

---
+++ Promies.js
@@ -0,0 +1,22 @@
+A Promise is an object representing the eventual completion or failure of an asynchronous operation.
+  A Promise is an Object that links Producing code and Consuming code
+function myDisplayer(some) {
+    document.getElementById("demo").innerHTML = some;
+  }
+  
+  let myPromise = new Promise(function(myResolve, myReject) {
+    let x = 0;
+  
+  // The producing code (this may take some time)
+  
+    if (x == 0) {
+      myResolve("OK ,Kavya");
+    } else {
+      myReject("Error");
+    }
+  });
+  
+  myPromise.then(
+    function(value) {myDisplayer(value);},
+    function(error) {myDisplayer(error);}
+  );
