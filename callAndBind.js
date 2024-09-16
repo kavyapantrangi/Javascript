fa9f143065fd16a8a7020e1a828b7f5ad8810550
kavyapantrangi <126671211+kavyapantrangi@users.noreply.github.com>
2024-09-16 21:50:07 +0530

    Update callAndBind.js

--- callAndBind.js
+++ callAndBind.js
@@ -10,3 +10,17 @@ function invite(greeting1,greeting2)
 }
 invite.call(emp1,'Hello','How are u?');
 invite.call(emp2,'Hello','How are u?');
+
+
+
+//Apply() method invokes the function by passing the arguments as an array
+var emp1={fname:'kavya',lname:'pantrangi'};
+var emp2={fname:'ramesh',lname:'pantrangi'};
+function invite(greeting1,greeting2)
+{
+    console.log(greeting1+' '+this.fname+' '+this.lname );
+    
+    
+}
+invite.apply(emp1,['Hello','How are u?']);
+invite.apply(emp2,['Hello','How are u?']);
