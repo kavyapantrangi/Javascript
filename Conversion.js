de9f5e27f26798a850699ee07160b29865659ed0
kavyapantrangi <126671211+kavyapantrangi@users.noreply.github.com>
2024-10-05 23:24:57 +0530

    Create Conversion.js

---
+++ Conversion.js
@@ -0,0 +1,10 @@
+// implicit conversion-Coersion
+console.log('1'+2);//to got converted to string
+//1 got converted to number
+console.log('1'*2);
+
+//Explicit Convserion -Type casting
+console.log(Number('2'));
+//to convert the number to string
+console.log(String(2));
+
